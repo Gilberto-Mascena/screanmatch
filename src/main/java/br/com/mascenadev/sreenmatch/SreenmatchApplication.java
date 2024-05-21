e151431b5152f3cec7640171012011902045a2f9
Gilberto Mascena <gilbertomascena@gmail.com>
2024-05-21 14:59:38 -0300

    added service directory and ConsumeApi class

--- src/main/java/br/com/mascenadev/sreenmatch/SreenmatchApplication.java
+++ src/main/java/br/com/mascenadev/sreenmatch/SreenmatchApplication.java
@@ -1,5 +1,7 @@
 package br.com.mascenadev.sreenmatch;
 
+import br.com.mascenadev.sreenmatch.service.ConsumeApi;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +16,13 @@ public class SreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+		System.getProperty("OMDB_KEY");
+
+		ConsumeApi consumeApi = new ConsumeApi();
+		var json = consumeApi.getData("https://www.omdbapi.com/?i=tt3896198&apikey=" + dotenv.get("OMDB_KEY"));
+		System.out.println(json);
 	}
 }
---
+++ src/main/java/br/com/mascenadev/sreenmatch/service/ConsumeApi.java
@@ -0,0 +1,26 @@
+package br.com.mascenadev.sreenmatch.service;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumeApi {
+
+    public String getData(String address) {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(java.net.URI.create(address))
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        String json = response.body();
+        return json;
+    }
+}

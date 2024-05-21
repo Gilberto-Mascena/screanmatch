6af55e5e4be213d4379f0555e2ef231702d756b1
Gilberto Mascena <gilbertomascena@gmail.com>
2024-05-21 13:13:09 -0300

    first commit

---
+++ src/main/java/br/com/mascenadev/sreenmatch/SreenmatchApplication.java
@@ -0,0 +1,19 @@
+package br.com.mascenadev.sreenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+	}
+}

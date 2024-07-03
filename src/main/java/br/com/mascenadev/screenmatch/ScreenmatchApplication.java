7f8c0d8ad08f336aa65b9fe4238ad28821e3a436
Gilberto Mascena <gilbertomascena@gmail.com>
2024-07-03 16:30:48 -0300

    created the execution class, for web and added maven dependency

--- src/main/java/br/com/mascenadev/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/mascenadev/screenmatch/ScreenmatchApplication.java
@@ -1,29 +1,13 @@
 package br.com.mascenadev.screenmatch;
 
-import br.com.mascenadev.screenmatch.application.Main;
-import br.com.mascenadev.screenmatch.repository.SerieRepository;
-import jakarta.annotation.Resource;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-
-    @Resource
-    private SerieRepository serieRepository;
+public class ScreenmatchApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(ScreenmatchApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-
-        Main main = new Main(serieRepository);
-
-        System.out.println();
-        main.displaysMenu();
     }
 }
